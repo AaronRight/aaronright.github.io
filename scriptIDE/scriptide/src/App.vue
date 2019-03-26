b1259c3aa2ad0a3cb6681c7e186f9fa46459c764
chugunov-v <chugunov-v@haulmont.com>
2019-03-26 19:02:10 +0400

    try it with cli

---
+++ scriptIDE/scriptide/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-content>
+        <div class="resultContainer">
+          <v-layout>
+            <v-flex grow pa-1 ><Editor  class="item elevation-5"/> </v-flex>
+            <v-flex grow pa-2 ><Editor  class="item elevation-5"/> </v-flex>
+            <v-flex grow pa-3 ><Editor  class="item elevation-5"/> </v-flex>
+          </v-layout>
+        </div>
+   
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Editor from './components/Editor'
+
+export default {
+  name: 'App',
+  components: {
+    Editor
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ scriptIDE/scriptide/src/components/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-sheet height="424">
+      <table>
+        <tr> <td>1</td><td>123</td> </tr>
+        <tr> <td>2</td><td>223</td> </tr>
+        <tr> <td>3</td><td>333</td> </tr>
+        <tr> <td>4</td><td>433</td> </tr>
+      </table>
+  </v-sheet>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      
+    })
+  }
+</script>
+
+<style>
+
+</style>
